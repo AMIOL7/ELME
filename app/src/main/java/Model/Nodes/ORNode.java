3e85a512ef703967d0bbe387ce50ae32288a628c
Viktor Bicskei <114762083+bgdxqi@users.noreply.github.com>
2022-10-21 01:22:03 +0200

    Update ORNode.java

--- app/src/main/java/Model/Nodes/ORNode.java
+++ app/src/main/java/Model/Nodes/ORNode.java
@@ -3,11 +3,7 @@ package Model.Nodes;
 /**
  * Implements the logical OR gate.
  * 
-<<<<<<< HEAD
  * @author Viktor Bicskei
-=======
- * @author victh
->>>>>>> branch 'basic-nodes' of https://github.com/AMIOL7/ELME.git
  *
  */
 
