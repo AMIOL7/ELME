07f5f87349958ea4b3e226640b27885434716516
Shamanayev Andrey <amiol7@inf.elte.hu>
2022-10-25 20:59:30 +0200

    Make small fixes

--- app/src/main/java/ELME/View/CustomWheelListener.java
+++ app/src/main/java/ELME/View/CustomWheelListener.java
@@ -1,7 +1,6 @@
-package View;
+package ELME.View;
 
 import de.gurkenlabs.litiengine.Game;
-import de.gurkenlabs.litiengine.graphics.ICamera;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
@@ -12,22 +11,17 @@ import java.awt.event.MouseWheelListener;
  */
 public class CustomWheelListener implements MouseWheelListener{
     
-    private final ICamera cam;
     private static final float MILTIPLIER = (float)0.5;
 
     public CustomWheelListener() {
         super();
-        this.cam = Game.world().camera();
     }
     
     
 
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
-        System.out.println("Mouse clicks: "+e.getWheelRotation());
-        cam.setZoom(MILTIPLIER*e.getWheelRotation()+cam.getZoom(), 1);
-//        cam.update();
-        System.out.println("Zoom: "+cam.getZoom());
+        Game.world().camera().setZoom(MILTIPLIER*e.getWheelRotation()+Game.world().camera().getZoom(), 1);
     }
     
 }
